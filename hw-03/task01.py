62d11f51164d5d6b6016b8eaa858f46735bbb100
Igor Miroshnichenko <Igor_Miroshnichenko@epam.com>
2021-12-30 16:18:38 +0300

    Finished task 1 only

--- hw-03/task01.py
+++ hw-03/task01.py
@@ -19,6 +19,8 @@
   Исключение: неправильные типы: 1 аргумент
   Исключение: неправильные типы: 2, 3 аргументы"""
 
+from math import sqrt
+
 class PreconditionError(Exception):
     pass
 
@@ -26,6 +28,7 @@ class ComplexRootError(Exception):
     pass
 
 def solve(a,b,c):
+    """Function solves quadratic equation of the form ax^2+bx+c=0"""
     try:
         if a==0:
             raise PreconditionError("Исключение: нарушение предусловия a!=0")
@@ -35,6 +38,36 @@ def solve(a,b,c):
         if discr < 0:
             raise ComplexRootError("Исключение: комплексные корни с аргументами: {0}, {1}, {2}".format(a,b,c))
     except TypeError:
-        print("Исключение: неправильные типы: ")
+        error_argument = [False,False,False]
+        if not isinstance(a,(int,float)):
+            error_argument[0]=True
+        if not isinstance(b,(int,float)):
+            error_argument[1]=True
+        if not isinstance(c,(int,float)):
+            error_argument[2]=True
+        if error_argument.count(True) == 1:
+            arg_message = str(error_argument.index(True)+1) + ' аргумент'
+        else:
+            arg_message = ', '.join([str(i+1) for i, x in enumerate(error_argument) if x == True])+' аргументы'
+        err_msg = "Исключение: неправильные типы: " + arg_message
+        return err_msg
+    except PreconditionError as err:
+        return err.args[0]
+    except ComplexRootError as err:
+        return err.args[0]
 
-solve(1,0,1)
+    if discr == 0:
+        return str(-b/(2*a))
+    else:
+        x = [(-b+sqrt(discr))/(2*a),(-b - sqrt(discr))/(2*a)]
+        x.sort()
+        x = [str(i) for i in x]
+        return ', '.join(x)
+
+if __name__ == '__main__':
+    print(solve(1,2,1))
+    print(solve(0.25,-2.5,6))
+    print(solve(1,0,1))
+    print(solve(0,1,-2))
+    print(solve(1j,2,3))
+    print(solve(1,"2","3"))
