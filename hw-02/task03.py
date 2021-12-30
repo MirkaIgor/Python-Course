acc7e12a8b084f7407abe73bebbb2e8b06719f99
Igor Miroshnichenko <Igor_Miroshnichenko@epam.com>
2021-12-30 12:38:36 +0300

    Fixed 3rd task of homework

--- hw-02/task03.py
+++ hw-02/task03.py
@@ -10,43 +10,86 @@
 from random import choice
 
 class Moving:
+    """
+    Class represents moving parameters of Skiman
+
+    --------------------
+    Attributes:
+        speed
+            speed in m/s
+        direction
+            There are 3 posssible directions: West,South,East
+    Methods:
+        gen_direction()
+            Generates random of possible directions
+        toggle_speed()
+            Sets the speed by current direction
+        random_move()
+            Change randomly moving parameters
+        stop()
+            Stops the Skiman
+    """
     def __init__(self) -> None:
+        """Initialize Moving object"""
         self.speed=0
         self.direction='S'
 
     def gen_direction(self):
+        """Generates random of possible directions"""
         dirs = ['W','S','E']
         self.direction = choice(dirs)
 
     def toggle_speed(self):
+        """Sets the speed by current direction"""
         speed_data = {'W':1,'S':5,'E':1}
         self.speed = speed_data[self.direction]
 
     def random_move(self):
+        """Change randomly moving parameters"""
         self.gen_direction()
         self.toggle_speed()
 
     def stop(self):
+        """Stops the Skiman"""
         self.speed=0
         self.direction='S'
 
 class Skiman:
+    """
+    Class represents a Skiman
+
+    -----------------
+    Attributes:
+        name        str
+            Skiman's name
+        move        Moving
+            Skiman's parameters of moving, such as speed and direction
+    Methods:
+        start()
+            Start moving
+        stop()
+            Stops moving
+    """
     def __init__(self, name: str) -> None:
+        """Initialize Skiman object"""
         self.name = name
         self.move = Moving()
     
     def start(self):
+        """Start moving"""
         self.move.random_move()
 
     def stop(self):
+        """Stops moving"""
         self.move.stop()
 
 def main():
+    """Launch this function to see task result"""
     skiman = Skiman('Steve')
     skiman.start()
     distance = 0
     directions = []
-    for time_ in range(17):
+    for _ in range(17):
         distance+=skiman.move.speed
         directions.append(skiman.move.direction)
         skiman.move.random_move()
